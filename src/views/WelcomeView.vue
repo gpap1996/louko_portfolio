70515f545bf8e28e1d1b8a0690447f0c7d0d7018
Giorgos Papapanos <g.papapanos1996@gmail.com>
2024-02-08 18:56:10 +0200

    welcome page

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-screen h-screen bg-black flex items-center justify-center">
+    <button
+      @click="$router.push('/home')"
+      class="text-black bg-white text-[22px] py-2 px-6 rounded-lg cursor-pointer"
+    >
+      Enter Louko's Showcase
+    </button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
